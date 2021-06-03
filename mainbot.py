5d81b6ff9a7946179103be28799f32d73f533623
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2021-06-03 14:56:31 +0530

    Update mainbot.py

--- mainbot.py
+++ mainbot.py
@@ -4,14 +4,14 @@ import time
 import math
 import pyautogui
 
-# Screen Dimensions for screen of size 1920X1080
+# For screen of size of 1920X1080
 top, left, width, height = 293, 0, 700, 465
 
-# helper variables to calculate time
+
 last = 0
 total_time = 0
 
-# the intervals where the bot will search for obstacles
+
 y_search_cactus, x_start, x_end = 350, 350, 380
 y_search_bird = 275 # for the birds
 
@@ -21,14 +21,14 @@ while True:
     t1 = time.time()
 
 
-    # increase the search width every second to simulate the dino acceleration
+    
     if math.floor(total_time) != last:
         x_end += 3
         if x_end >= width:
             x_end = width
         last = math.floor(total_time)
 
-    #Get a screen shot
+    
     screenshot = gui.screenshot(region=(left,top, width, height))
     pixels = screenshot.load()
 
